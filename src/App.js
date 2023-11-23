a342fa9a452653f774291f286ed5c5e147e72a4c
navyasharma0203 <navyasharma0203@gmail.com>
2023-11-23 19:10:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./components/Login";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/"  element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
